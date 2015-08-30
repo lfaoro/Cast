da2f73ac0bc22daf0ad2015c5d2a5504800ff383
Leonard <lfaoro@me.com>
2015-08-30 18:10:55 +0200

    Implemented `putInPasteboard`

--- Cast/PasteboardClient.swift
+++ Cast/PasteboardClient.swift
@@ -16,43 +16,54 @@ enum PBItem {
 	case File(NSURL) //to Dropbox/iCloud
 }
 
-func getPasteboardItems() -> Observable<PBItem> {
-	let pb = NSPasteboard.generalPasteboard()
+class PasteboardClient {
 
-	let classes: [AnyClass] = [NSURL.self,NSString.self,NSAttributedString.self,NSImage.self]
-	let options: [String: AnyObject] = [ :
-		//            NSPasteboardURLReadingFileURLsOnlyKey: NSNumber(bool: true),
-		//            NSPasteboardURLReadingContentsConformToTypesKey: NSImage.imageTypes(),
-	]
+	class func putInPasteboard(items items: [String]) -> Bool {
+		let pb = NSPasteboard.generalPasteboard()
 
-	let copiedItems = pb.readObjectsForClasses(classes, options: options)
+		pb.clearContents()
 
-	return create { stream in
-		if let items = copiedItems {
+		return pb.writeObjects(items)
+	}
 
-			for item in items {
-				if let image = item as? NSImage {
-					sendNext(stream, .Image(image))
+	class func getPasteboardItems() -> Observable<PBItem> {
+		let pb = NSPasteboard.generalPasteboard()
+
+		let classes: [AnyClass] = [NSURL.self,NSString.self,NSAttributedString.self,NSImage.self]
+		let options: [String: AnyObject] = [ :
+			//            NSPasteboardURLReadingFileURLsOnlyKey: NSNumber(bool: true),
+			//            NSPasteboardURLReadingContentsConformToTypesKey: NSImage.imageTypes(),
+		]
+
+		let copiedItems = pb.readObjectsForClasses(classes, options: options)
+
+		return create { stream in
+			if let items = copiedItems {
+
+				for item in items {
+					if let image = item as? NSImage {
+						sendNext(stream, .Image(image))
+					}
+
+					if let text = item as? NSString {
+						sendNext(stream, .Text(String(text)))
+					}
+
+					if let attrText = item as? NSAttributedString {
+						sendNext(stream, .Text(attrText.string))
+					}
+
+					if let file = item as? NSURL {
+						sendNext(stream, .File(file))
+					}
 				}
 
-				if let text = item as? NSString {
-					sendNext(stream, .Text(String(text)))
-				}
-
-				if let attrText = item as? NSAttributedString {
-					sendNext(stream, .Text(attrText.string))
-				}
-
-				if let file = item as? NSURL {
-					sendNext(stream, .File(file))
-				}
+				sendCompleted(stream)
 			}
 
-			sendCompleted(stream)
+			sendError(stream, PBError.UnreadableData)
+
+			return NopDisposable.instance
 		}
-
-		sendError(stream, PBError.UnreadableData)
-
-		return NopDisposable.instance
 	}
 }
